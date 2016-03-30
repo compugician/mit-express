6cba0569df54be5a4496f558efa3c50837d548bd
Tal Achituv <tal@compugician.com>
2016-03-30 17:41:11 -0400

    black bg, jqueryUI, draggable perspective corners started

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,10 +3,29 @@
 // socket io
 var socket = io();
 
+const MAX_X = 620;
+const MAX_Y = 620;
+
+const TL_X = 290;
+const TL_Y = 37;
+const TR_X = 824;
+const TR_Y = 126;
+const BL_X = 321;
+const BL_Y = 738;
+const BR_X = 836;
+const BR_Y = 704;
+const BOX_SIZE = 20;
+
+
 socket.on('point-client', function (data) {
-	// convert coordinates from steps to pixels
-	data.x = Math.floor(data.x/5.5);
-	data.y = Math.floor(data.y/5.5);
+  // convert coordinates from steps to pixels
+  data.x = $("#topleft").offset().left+BOX_SIZE/2 + (data.x/MAX_X) * ($("#topright").offset().left - $("#topleft").offset().left - BOX_SIZE);
+  data.y = $("#topleft").offset().top+BOX_SIZE/2 + ((MAX_Y-data.y)/MAX_Y) * ($("#bottomleft").offset().top - $("#topleft").offset().top - BOX_SIZE);
+
+
+  //TODO:
+  //var transform = PerspT([0, 0, w, 0, 0, h, w, h], [x1, y1, x2, y2, x3, y3, x4, y4]);
+
   queue.push(data);
 });
 
@@ -21,12 +40,41 @@ var queue = [];
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+
+  createDiv('').id("topleft").class("draggable");
+  createDiv('').id("topright").class("draggable");
+  createDiv('').id("bottomleft").class("draggable");
+  createDiv('').id("bottomright").class("draggable");
+
+
+  $( ".draggable" ).draggable();
+  $( "#topleft" ).position({
+    my:"bottom right",
+    at:"left+"+TL_X+" top+"+TL_Y,
+    of:"body"
+  });
+  $( "#topright" ).position({
+    my:"bottom left",
+    at:"left+"+TR_X+" top+"+TR_Y,
+    of:"body"
+  });
+  $( "#bottomleft" ).position({
+    my:"top right",
+    at:"left+"+BL_X+" top+"+BL_Y,
+    of:"body"
+  });
+  $( "#bottomright" ).position({
+    my:"top left",
+    at:"left+"+BR_X+" top+"+BR_Y,
+    of:"body"
+  });
+
 }
 
 var pointBegin, point1, point2, pointEnd;
 function draw() {
   clear();
-  background('white');
+  background('black');
   
   // delete oldest points
   while (queue.length > tailLength) queue.shift();
@@ -36,14 +84,14 @@ function draw() {
     point1 = queue[queue.length - 1];
     noStroke();
 
-	//create a black outline for the airbrush head.
-	fill(0, 0, 0, 255);
-	ellipse(point1.x, point1.y, point1.dotDiameter*11/10, point1.dotDiameter*11/10);
-	fill(255, 255, 255, 255);
+  //create a black outline for the airbrush head.
+  fill(0, 0, 0, 255);
+  ellipse(point1.x, point1.y, point1.dotDiameter*11/10, point1.dotDiameter*11/10);
+  fill(255, 255, 255, 255);
 
     ellipse(point1.x, point1.y, point1.dotDiameter, point1.dotDiameter);
-	
-	//fill based on fill opacity and color
+  
+  //fill based on fill opacity and color
     fill(point1.color.r, point1.color.g, point1.color.b, point1.color.a);
     ellipse(point1.x, point1.y, point1.dotDiameter, point1.dotDiameter);
 

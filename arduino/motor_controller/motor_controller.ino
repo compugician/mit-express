a011df031a854f147fec0e1c5ba39ea9509441ca
Tal Achituv <tal@achituv.com>
2016-04-08 14:13:25 -0400

    latest dynamixel control. Added flow logic for paint on/off, faster motion, re-calibrated positions, etc.

--- arduino/motor_controller/motor_controller.ino
+++ arduino/motor_controller/motor_controller.ino
@@ -2,21 +2,22 @@
 #include "DynamixelMotor.h"
 
 // clearpath vars
-int enable = 9;
-int dirpin = 10;
-int steppin = 11;
+int scopePin = 13;
+int timeoutSetPin = 12;
+
+int paintflowTime = 300; //Time in ms for paintflow to go 0%<->100%
 
 //dynamixel vars
-int16_t speed=256;
+int16_t motorSpeed=1023;//WAS 256
 
-DynamixelInterface &interface=*createSoftSerialInterface(2,3); // RX, TX
+DynamixelInterface &interface=*createSerialInterface(Serial1); // RX, TX
 
-int cwlimit1 = 65;
-int ccwlimit1 = 915;
-int cwlimit2 = 870;
-int ccwlimit2 = 880;
-int cwlimit3 = 1;
-int ccwlimit3 = 262;
+int cwlimit1 = 28;
+int ccwlimit1 = 851;
+int cwlimit2 = 920;
+int ccwlimit2 = 929;
+int cwlimit3 = 575;
+int ccwlimit3 = 1023;
 
 DynamixelMotor motor1(interface, 1);
 DynamixelMotor motor2(interface, 2);
@@ -26,36 +27,36 @@ DynamixelMotor motor3(interface, 3);
 void setup() {
   Serial.begin(115200);
 
-  //clearpath init
-  pinMode(dirpin, OUTPUT);
-  pinMode(steppin, OUTPUT);
-  pinMode(enable, OUTPUT);
-  digitalWrite(enable, HIGH);
-  delay(100);
+  Serial.println("Starting...");
+  
+  pinMode(scopePin, OUTPUT);
+  pinMode(timeoutSetPin, INPUT_PULLUP);
 
   //dynamixel init
-  interface.begin(57142);
-  delay(100);
+//  interface.begin(57142);
+  interface.begin(250000);
+  delay(50);
 
   initializeMotor(motor1, cwlimit1, ccwlimit1);
   initializeMotor(motor2, cwlimit2, ccwlimit2);
   initializeMotor(motor3, cwlimit3, ccwlimit3);
-  
-  delay(350);
-  Serial.println("Ready!");
+
+  delay(10);
+  Serial.print("Ready! ");
+  Serial.println(millis());
 }
 
 void initializeMotor(DynamixelMotor motor, int cwlimit, int ccwlimit){
   motor.init();
   motor.enableTorque();
-  motor.speed(speed);
+  motor.speed(motorSpeed);
   motor.jointMode(cwlimit, ccwlimit);
   
   for (int i = 0; i < 2; i++) {
     motor.led(true);
-    delay(100);
+    delay(10);
     motor.led(false);
-    delay(100);
+    delay(10);
   }
 }
 
@@ -69,61 +70,87 @@ void paintEnable(boolean on) {
   }
 }
 
-// each index is a half step
-void colorSelect(int index) {
-  if (index < 0 || index >= 17) return;
-  motor1.position(cwlimit1 + (index * ((ccwlimit1 - cwlimit1)/16)));
+//changed from half-step index to a float, since different color pairs have different 'middle' point.
+void colorSelect(float index) {
+  if (index < 0 || index > 8) return;
+  motor1.position(cwlimit1 + (index/8.0 * ((ccwlimit1 - cwlimit1))));
 }
 
 void flowSet(int percent) {
   if (percent < 0 || percent > 100) return;
   if (percent == 0) {
-    motor3.position(ccwlimit3);
+    motor3.position(cwlimit3); //250->740
   }
   else {
-    motor3.position(ccwlimit3 - (percent * (ccwlimit3 - cwlimit3)/100));
+    int newPosition = cwlimit3 + (percent * ((ccwlimit3 - cwlimit3)/100.0));
+    motor3.position(newPosition);
   }
 }
 
 unsigned long lastReceivedAt;
 int stepScale = 8;
 
+int lastFlow = 0;
+
+void shutoffPaint() {
+    flowSet(0); 
+    float factor = (lastFlow/100.0); 
+    int tWait = factor * paintflowTime;        
+    delay(tWait); // a factor of the change in flow
+    paintEnable(false);
+//    Serial.println(tWait);
+}
+
+
+
 void loop() {
   // 5 second timeout if no serial command received
-  if (millis() - lastReceivedAt > 5000) {
-      flowSet(0);
-      delay(100);
-      paintEnable(false);
+  if (digitalRead(timeoutSetPin)==LOW) {
+    if (millis() - lastReceivedAt > 300) { //TODO: make this conditional by switch
+      shutoffPaint();
+    }
   }
 
   while (Serial.available() > 0) {
-    Serial.print("Delta: ");
-    long delta = millis() - lastReceivedAt;
-    Serial.println(delta);
+
+//    Serial.print("Delta: ");
+//    long delta = millis() - lastReceivedAt;
+//    Serial.println(delta);
+
     lastReceivedAt = millis();
-    // parse out values
-    if (Serial.read()!='^') {
-      flushSerial();
-    } else {
+    while (Serial.available() < 8) { }
+    
+    while (Serial.available() > 0 && Serial.read()!='^') {
+      //forward till caret
+    }
+    
+    digitalWrite(scopePin,HIGH);
+    if (Serial.available() > 0) {
       int x = Serial.parseInt();
       int y = Serial.parseInt();
-      int colorIdx = Serial.parseInt();
+      float colorIdx = Serial.parseFloat();
       int flow = Serial.parseInt();
-      Serial.println(flow);
-  
+      //Serial.println(flow);
       if (Serial.read() == '\n') {
   //    dynamixel signals
         colorSelect(colorIdx);
-        delay(10);
-        paintEnable(flow > 0); // in case it was off, turn it on before flow
-        delay(10);
-        flowSet(flow);
-        delay(10);
-        paintEnable(flow > 0); // if it was on, turn it off after flow
+        delay(1);
+        if (flow>0) {
+          paintEnable(true);
+          delay(10); //this can be tuned.
+          flowSet(flow);
+        } else {
+          //flow is 0;
+          shutoffPaint();
+        }
+
+        lastFlow = flow;
       } else {
         flushSerial();
-      } 
-    } 
+      }
+       
+    }
+    digitalWrite(scopePin,LOW);
   }
 }
 
